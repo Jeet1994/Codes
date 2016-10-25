66f925f08be7f1a588a0b6893239161213c839f9
Jeet1994 <pragyaditya.das94@gmail.com>
2016-10-25 11:42:30 +0530

    Added r stuff

---
+++ SolvingRcranInstallProblem/run.R
@@ -0,0 +1,10 @@
+##WHEN YOU GET THE FOLLOWING ERROR:
+
+#Warning: unable to access index for repository http://cran.rstudio.com/src/contrib:
+#cannot open URL 'http://cran.rstudio.com/src/contrib/PACKAGES'
+#Warning: unable to access index for repository http://www.stats.ox.ac.uk/pub/RWin/src/contrib:
+  #cannot open URL 'http://www.stats.ox.ac.uk/pub/RWin/src/contrib/PACKAGES'
+
+#Run the following:
+
+options(download.file.method="libcurl")
