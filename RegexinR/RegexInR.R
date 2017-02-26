a22c09e7a7bcbe6395890d55a273adf03ce510dc
Pragyaditya Das <Pragyaditya Das>
2017-02-26 12:05:14 +0530

    Regular expressions -used gsun and chractr

---
+++ RegexinR/RegexInR.R
@@ -0,0 +1,12 @@
+x <- c("This is a sentence about axis", "This is shit")
+gsub("t","LALA",x)
+gsub("T", "HAHA", x)
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+z <- c("www.gmail.com", "www.yahoo.com")
+y <- gsub("www", "aaa",z)
+m <- chartr("wwwcom", "aaaaaa", z)
+
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
